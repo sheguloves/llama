0c9eb72fa39432f3665a5e9dbd9b3e34dcffaa5c
Guofeng Xi <guofengx@vmware.com>
2024-02-29 15:19:08 +0800

    Update shell script

--- example_chat_completion.py
+++ example_chat_completion.py
@@ -10,11 +10,11 @@ from llama import Llama, Dialog
 
 def main(
     ckpt_dir: str,
-    tokenizer_path: str,
+    tokenizer_path: str = "tokenizer.model",
     temperature: float = 0.6,
     top_p: float = 0.9,
     max_seq_len: int = 512,
-    max_batch_size: int = 8,
+    max_batch_size: int = 6,
     max_gen_len: Optional[int] = None,
 ):
     """
--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ run() {
       $@
     done
 }
-run 20 torchrun --nproc_per_node 1 example_chat_completion.py --ckpt_dir llama-2-7b-chat/ --tokenizer_path tokenizer.model --max_seq_len 512 --max_batch_size 6
+run $1 torchrun --nproc_per_node 1 example_chat_completion.py --ckpt_dir llama-2-7b-chat/ 
